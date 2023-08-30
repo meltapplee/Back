b5aa3d1fcd6f8af49d46ee579db0197b7a3726a4
soohyeon <qpsxhf410@outlook.kr>
2023-08-30 23:31:27 +0900

    ✨:: User엔티티 작성

---
+++ post-study/src/main/java/com/example/poststudy/domain/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.poststudy.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String accountId, String nickName, String password) {
+        this.accountId = accountId;
+        this.nickName = nickName;
+        this.password = password;
+    }
+}
