326cdf658bcfefddd80b9ec1c6c9f55975daa92e
soohyeon <qpsxhf410@outlook.kr>
2023-11-08 10:30:38 +0900

    🔨 ::post-comment 연관관계 설정

--- post-study/src/main/java/com/example/poststudy/domain/post/domain/Post.java
+++ post-study/src/main/java/com/example/poststudy/domain/post/domain/Post.java
@@ -1,5 +1,6 @@
 package com.example.poststudy.domain.post.domain;
 
+import com.example.poststudy.domain.comment.domin.Comment;
 import com.example.poststudy.domain.post.domain.type.ThemeType;
 import com.example.poststudy.domain.user.domain.User;
 import lombok.AccessLevel;
@@ -7,6 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -16,8 +18,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -45,6 +50,9 @@ public class Post {
     @JoinColumn(name = "accountId")
     private User user;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<>();
+
     @Builder
     public Post(User user, String title, ThemeType theme, String content) {
         this.user = user;
@@ -54,9 +62,10 @@ public class Post {
         this.createDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy년 M월 d일"));
     }
 
-    public void update(String title, ThemeType theme, String content) {
+    public Long update(String title, ThemeType theme, String content) {
         this.title = title;
         this.theme = theme;
         this.content = content;
+        return this.id;
     }
 }
--- post-study/src/main/java/com/example/poststudy/domain/post/presentation/dto/response/PostResponse.java
+++ post-study/src/main/java/com/example/poststudy/domain/post/presentation/dto/response/PostResponse.java
@@ -1,7 +1,11 @@
 package com.example.poststudy.domain.post.presentation.dto.response;
 
+import com.example.poststudy.domain.comment.domin.Comment;
+import com.example.poststudy.domain.comment.presentation.response.CommentResponse;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class PostResponse {
@@ -11,13 +15,15 @@ public class PostResponse {
     private final String title;
     private final String content;
     private final String createDate;
+    private final List<CommentResponse> comments;
 
     @Builder
-    public PostResponse(String nickname, String theme, String title, String content, String createDate){
+    public PostResponse(String nickname, String theme, String title, String content, String createDate, List<CommentResponse> comments){
         this.nickname = nickname;
         this.theme = theme;
         this.title = title;
         this.content = content;
         this.createDate = createDate;
+        this.comments = comments;
     }
 }
